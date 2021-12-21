5e1f9cd4ffa1293e7f7476bb1238525623a7de7a
drigoor <>
2021-12-21 20:12:11 +0000

    misc things (mainly temporary things)

--- src/clj_asteroids/core.clj
+++ src/clj_asteroids/core.clj
@@ -3,14 +3,89 @@
             [quil.middleware :as m]))
 
 
-(defn setup-state []
-  (q/frame-rate 60)
-  (q/color-mode :hsb)
-  (q/smooth)
+;; https://ostash.dev/posts/
+;;    e.g.: Data notation in Clojure
+;; (let [uuid #uuid "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"]
+;; 	(class uuid))
+
+
+;; ;; from: http://makble.com/how-to-generate-uuid-in-clujure
+;; (defn show [ t ]
+;;   (map #(println %) (seq (:declaredMethods (bean t)))))
+;; ;; (show java.util.UUID)
+
+
+(defn create-entity []
+  (java.util.UUID/randomUUID))
+
+
+
+(component renderable [func]
+           :fn func)
+
+
+(defn renderer [renderables]
+  (doseq [e renderables]
+    (let [rend (as e :renderable)]
+      ((rend :fn) e))))
+
+
+(defmacro component [name params & body]
+  `(defn ~name ~params
+     {~(keyword name) ~(apply hash-map body)}))
+
+
+(component position [x y]
+           :x x
+           :y y)
+
+
+(component health [lives]
+           :lives lives
+           :health 100)
+
+
+(defmacro entity [name & body]
+  `(merge {:name ~(keyword name)} ~@body))
+
+
+;; name is a keyword
+(defn entity [name & body]
+  (apply merge {:name name} body))
+
+
+(entity :player
+        (position 100 100)
+        (health 3))
+
+
+(let [player (entity :player
+                     (position 100 100)
+                     (health 3))
+      {x :x y :y} (:position player)]
+  (println "player position: " x ))
+
+
+;; ---------------------------------------------------------------------
+
+
+
+
+;; based on: http://www.quil.info/sketches/show/example_nanoscopic
+
+
+(defn create-game-state []
   {:x 0
    :y 0
    :x-speed 1
    :y-speed 3.3})
+
+
+(defn setup-state []
+  (q/frame-rate 60)
+  (q/color-mode :rgb)
+  (q/smooth)
+  (create-game-state))
 
 
 (defn update-state [state]
@@ -34,20 +109,35 @@
 
 
 (defn draw-state [state]
-  (q/background 240)
-  (q/fill 255)
-  (q/stroke 127)
-  (q/stroke-weight 2)
-  (q/ellipse (:x state) (:y state) 48 48)
-  (q/fill 0)
-  ;; from: http://quil.info/api/input/keyboard
-  (doseq [[ind capt fn] [[0 "key-as-keyword" q/key-as-keyword]
-                         [1 "key-code" q/key-code]
-                         [2 "key-coded?" (fn* [] (q/key-coded? (q/raw-key)))]
-                         [3 "key-pressed?" q/key-pressed?]
-                         [4 "raw-key" q/raw-key]
-                         [5 "key-modifiers" q/key-modifiers]]]
-    (q/text (str capt " " (fn)) 10 (+ (* 20 ind) 20))))
+  (q/background 246 245 236)
+                                        ; black
+  (q/fill (q/color 246 245 236))
+  (q/rect 1 1 100 100)
+                                        ; semitransparent gray
+  (q/fill (q/color 128 128))
+  (q/rect 70 70 100 100)
+                                        ; purple
+  (q/fill (q/color 255 0 255))
+  (q/rect 140 140 100 100)
+                                        ; semitransparent cyan
+  (q/fill (q/color 0 255 255 120))
+  (q/rect 210 210 100 100)
+  
+  ;; (q/fill (q/color 246 245 236))
+  ;; (q/ellipse (:x state) (:y state) 48 48)
+  ;; (q/stroke 127)
+  ;; (q/stroke-weight 2)
+  ;; (q/ellipse (:x state) (:y state) 48 48)
+  ;; (q/fill 0)
+  ;; ;; from: http://quil.info/api/input/keyboard
+  ;; (doseq [[ind capt fn] [[0 "key-as-keyword" q/key-as-keyword]
+  ;;                        [1 "key-code" q/key-code]
+  ;;                        [2 "key-coded?" (fn* [] (q/key-coded? (q/raw-key)))]
+  ;;                        [3 "key-pressed?" q/key-pressed?]
+  ;;                        [4 "raw-key" q/raw-key]
+  ;;                        [5 "key-modifiers" q/key-modifiers]]]
+  ;;   (q/text (str capt " " (fn)) 10 (+ (* 20 ind) 20)))
+  )
 
 
 (defn key-pressed [state event]
@@ -82,5 +172,4 @@
   :draw draw-state
   :key-pressed key-pressed
   :key-released key-released
-  :features [:keep-on-top]
   :middleware [m/fun-mode])
